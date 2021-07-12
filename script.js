46f9e080d61dc4a384f684ebe441ce7cd6d70dd6
Andrei Bodrov <63089699+bas132828@users.noreply.github.com>
2021-07-12 21:08:11 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,61 @@
+"use strict";
+const btn = document.querySelector(".btn");
+const form = document.getElementById("form");
+//need this for setting cookies expiration date for tomorrow
+const date = new Date(Date.now() + 86400e3).toUTCString();
+
+//checking if we get some cookies to render
+function getCookie(name) {
+  let matches = document.cookie.match(
+    new RegExp(
+      "(?:^|; )" +
+        name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, "\\$1") +
+        "=([^;]*)"
+    )
+  );
+  return matches ? decodeURIComponent(matches[1]) : undefined;
+}
+
+const arr = ["surname", "name", "patronomic", "date", "place", "passport"];
+
+arr.forEach((el) => {
+  //assigning the value of the cookie if we get one
+  getCookie(el)
+    ? (document.querySelector(`.${el}`).value = getCookie(el)
+        .split(",")[0]
+        .slice(0, -3))
+    : //assigning empty string otherwise
+      (document.querySelector(`.${el}`).value = "");
+});
+
+const cookieSetter = (key, val) => {
+  document.cookie = `${key}=${val}` + date;
+};
+
+const fullSubmission = (e) => {
+  const surname = document.querySelector(".surname");
+  const name = document.querySelector(".name");
+  const patronomic = document.querySelector(".patronomic");
+  const date = document.querySelector(".date");
+  const place = document.querySelector(".place");
+  const passport = document.querySelector(".passport");
+  e.preventDefault();
+  //setting user ready for JSON.stringify
+  const user = {
+    surname: surname.value.trim(),
+    name: name.value.trim(),
+    patronomic: patronomic.value.trim(),
+    date: date.value,
+    place: place.value.trim(),
+    passport: passport.value.trim(),
+  };
+  // using Map to loop over user props
+  const userMap = new Map(Object.entries(user));
+  userMap.forEach((value, key) => {
+    cookieSetter(key, value);
+  });
+
+  return console.log(JSON.stringify(user));
+};
+
+form.addEventListener("submit", fullSubmission);
